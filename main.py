a30258f819d59aa6198cf8cbf75fd5882cbf4db7
Ilya Tolokonov <itolokonov@mail.ru>
2022-05-12 17:56:07 +0300

    Site for style transfer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 import streamlit as st
+from style_algorithm import style_transfer
 from PIL import Image
 st.set_page_config(page_icon=Image.open('media/logo.png'), layout="wide")
 st.title('A Neural Algorithm of Artistic Style')
@@ -14,10 +16,8 @@ with col1:
     if mode_col1 == 'Loading from your device':
         uploaded_content = st.file_uploader("Choose a content image", type=["png", "jpg"])
         if uploaded_content is not None:
-            pass
-               
-            #image = Image.open('sunrise.jpg')
-            #st.image(image, caption='Sunrise by the mountains')
+            st.image(uploaded_content, caption='Content image')
+            img_content = Image.open(uploaded_content, mode='r')
 
     else:
         content_url = st.text_input('Link field', 'Paste your URL to image')
@@ -32,10 +32,8 @@ with col2:
 
         uploaded_style = st.file_uploader("Choose a style image", type=["png", "jpg"])
         if uploaded_style is not None:
-            pass
-
-            #image = Image.open('sunrise.jpg')
-            #st.image(image, caption='Sunrise by the mountains')
+            st.image(uploaded_style, caption='Style image')
+            img_style = Image.open(uploaded_style, mode='r')
     else:
         style_url = st.text_input('Link field', 'Paste your URL to image')
         st.write('URL is correct')
@@ -46,18 +44,19 @@ intensity = st.radio(
     ('Low', 'Medium', 'High'))
 
 if intensity == 'Medium':
-    st.write('You selected medium intensity.')
+    style_weight = 10000000
+elif intensity == 'High':
+    style_weight = 1000000000
 else:
-    st.write("You didn't select default intensity.")
+    style_weight = 100000
 
-    count_of_iterations = st.slider('We recommend one iteration of algorithm by default. With more iterations waiting time will increase', 1, 50, 1)
-    st.write("I choose ", count_of_iterations, ' iterations')
+#count_of_iterations = st.slider('We recommend one iteration of algorithm by default. With more iterations waiting time will increase', 1, 50, 1)
+#n_iters = count_of_iterations
 
-if st.button('Generate') and uploaded_style and uploaded_content:
-    pass
-        #result = transfer_style(uploaded_content, uploaded_style)
-        #st.image(result)
-        #result.save(f'result_images/{str(random.randint(0, 10000))}.png')
+if st.button('Generate') and img_style and img_content:
+    result = style_transfer(img_content, img_style, style_weight)
+    st.image(result)
+    #result.save(f'result_images/{str(random.randint(0, 10000))}.png')
 
 
 
---
+++ style_algorithm.py
@@ -0,0 +1,123 @@
+import numpy as np
+from copy import deepcopy
+
+import PIL
+from PIL import Image
+
+import torch
+import torch.nn.functional as F
+from torchvision import models
+from torchvision import transforms
+
+def style_transfer(img_content, img_style, style_weight = 10000000, n_iters = 1):
+    
+    img_style = img_style.resize((224, 224), Image.ANTIALIAS)
+
+
+    content_height, content_width = img_content.size[0], img_content.size[1]
+    img_content = img_content.resize((224, 224), Image.ANTIALIAS)
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    def to_tensor(img):
+        transform = transforms.ToTensor()
+        return transform(img)[None]
+
+    def to_image(tensor):
+        img = tensor[0].detach().cpu().numpy()
+        img = img.transpose(1, 2, 0)
+        return np.clip(img, 0, 1)
+
+    style_tensor = to_tensor(img_style).to(device).float()
+    content_tensor = to_tensor(img_content).to(device).float()
+
+    model = models.vgg16_bn(pretrained=True).to(device)
+    model.eval()
+
+    class ForwardHooks:
+        def __init__(self):
+            self.activation_dict = {}
+        
+        def get_hook(self, name):
+            def hook(model, input, output):
+                nonlocal self
+                self.activation_dict[name] = output
+            return hook
+
+    def content_loss_fn(activations1, activations2):
+        return F.mse_loss(activations1, activations2)
+
+    def build_gramm_matrix(activation):
+        _, c, w, h = activation.size()
+        features = activation.view(c, w*h)
+        G = torch.mm(features, features.t())
+        return G.div(c * w * h)
+
+    def style_loss_fn(activations1, gramm_matix2):
+        gramm_matix1 = build_gramm_matrix(activations1)
+        return F.mse_loss(gramm_matix1, gramm_matix2)
+
+    def get_activations(inputs, hooks, activation_names):
+        return [hooks.activation_dict[feature_name] for feature_name in activation_names]
+
+    hooks = ForwardHooks()
+    for name, module in model.named_modules():
+        module.register_forward_hook(hooks.get_hook(name))
+
+    content_activation_names = ['features.10']
+    style_activation_names = ['features.0', 'features.3', 'features.7', 'features.10', 'features.14']
+
+    with torch.no_grad():
+        model(content_tensor)
+        content_target_activations = get_activations(content_tensor, hooks, content_activation_names)
+
+        model(style_tensor)
+        style_target_activations = get_activations(style_tensor, hooks, style_activation_names)
+        
+        style_target_gramm_matrixes = [
+            build_gramm_matrix(activation) for activation in style_target_activations
+        ]
+
+    for param in model.parameters():
+        param.requires_grad = False
+    
+    optimization_tensor = content_tensor.clone().contiguous()
+    optimization_tensor.requires_grad=True
+    optimizer = torch.optim.LBFGS([optimization_tensor])
+
+    content_weight = 1 #frozen hyperparameter
+
+    for i in range(n_iters):
+        
+        def closure():
+            model(optimization_tensor)
+            content_activations = get_activations(optimization_tensor, hooks, content_activation_names)
+            style_activations = get_activations(optimization_tensor, hooks, style_activation_names)
+
+            content_losses = [
+                content_loss_fn(activation1, activation2)
+                for activation1, activation2 in zip(content_activations, content_target_activations)
+            ]
+            content_loss = sum(content_losses) * content_weight
+            
+
+            style_losses = [
+                style_loss_fn(activation1, gramm2)
+                for activation1, gramm2 in zip(style_activations, style_target_gramm_matrixes)
+            ]
+            style_loss = sum(style_losses) * style_weight
+
+            loss = content_loss + style_loss
+                
+            optimizer.zero_grad()
+            loss.backward()
+            
+            return loss
+        optimizer.step(closure)
+
+    opt_tensor_numpy = optimization_tensor[0].cpu().detach().numpy()
+    pil_image = Image.fromarray(np.array(np.clip(opt_tensor_numpy.transpose(1, 2, 0), 0, 1) * 255, dtype=np.uint8))
+    resized = pil_image.resize((content_height, content_width), Image.Resampling.BICUBIC)
+
+    #resized.save('output.jpg')
+    return resized
