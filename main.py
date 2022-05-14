9bcf834059e9bd10d6be746f9cf1ecc378a6bcfb
Ilya Tolokonov <itolokonov@mail.ru>
2022-05-14 14:23:02 +0300

    Loding via link

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
+import shutil
+import requests
 import streamlit as st
 from style_algorithm import style_transfer
 from PIL import Image
+import numpy as np
 st.set_page_config(page_icon=Image.open('media/logo.png'), layout="wide")
 st.title('A Neural Algorithm of Artistic Style')
 st.subheader('To transfer a style, just select the content image you want to change and select the style image which patterns you want to transfer')
@@ -17,11 +20,22 @@ with col1:
         uploaded_content = st.file_uploader("Choose a content image", type=["png", "jpg"])
         if uploaded_content is not None:
             st.image(uploaded_content, caption='Content image')
-            img_content = Image.open(uploaded_content, mode='r')
+            img_content = Image.open(uploaded_content, mode='r').convert('RGB')
 
     else:
         content_url = st.text_input('Link field', 'Paste your URL to image')
-        st.write('URL is correct')
+        if content_url is not 'Paste your URL to image':
+            try:
+                response = requests.get(content_url, stream=True)
+                with open('img_content.png', 'wb') as out_file:
+                    shutil.copyfileobj(response.raw, out_file)
+
+                    img_content = Image.open('img_content.png', mode='r').convert('RGB')
+                    st.image(img_content, caption='Content image')
+                del response
+            except Exception as e:
+                e = RuntimeError('Unavalible link')
+                st.exception(e)
 
 with col2:
     mode_col2 = st.radio(
@@ -33,10 +47,21 @@ with col2:
         uploaded_style = st.file_uploader("Choose a style image", type=["png", "jpg"])
         if uploaded_style is not None:
             st.image(uploaded_style, caption='Style image')
-            img_style = Image.open(uploaded_style, mode='r')
+            img_style = Image.open(uploaded_style, mode='r').convert('RGB')
     else:
-        style_url = st.text_input('Link field', 'Paste your URL to image')
-        st.write('URL is correct')
+        style_url = st.text_input('Link field', 'Paste your URL to image', key="222")
+        if style_url is not 'Paste your URL to image':
+            try:
+                response = requests.get(style_url, stream=True)
+                with open('img_style.png', 'wb') as out_file:
+                    shutil.copyfileobj(response.raw, out_file)
+
+                    img_style = Image.open('img_style.png', mode='r').convert('RGB')
+                    st.image(img_style, caption='Style image')
+                del response
+            except Exception as e:
+                e = RuntimeError('Unavalible link')
+                st.exception(e)
 
      
 intensity = st.radio(
@@ -57,7 +82,6 @@ if st.button('Generate') and img_style and img_content:
     progress_bar = st.progress(0)
     
     result = style_transfer(img_content, img_style, progress_bar, style_weight, n_iters)
-
     st.image(result)
 
 
